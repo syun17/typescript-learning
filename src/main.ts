114b6c5bc60d96d22090457e6c4f6440f27cf92b
syun17 <shunki.s777@gmail.com>
2024-12-26 11:21:04 +0900

    typescriptの勉強

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+function add(a: number,b: number){
+    return a + b;
+}
+
+console.log(add(2,3))
+console.log(add(-3,2))
+// console.log(add("A","B"))
+console.log(add(parseInt("1"),parseInt("2")))
